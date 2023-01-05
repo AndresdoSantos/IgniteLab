aa2f2ff5adbb6435b497f656309e0928fc613c74
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-01-04 21:40:41 -0300

    feat(lab-01 & lab-04): all lessons about Ignite Lab 01 & 04

---
+++ lab-01/classroom/src/http/http.module.ts
@@ -0,0 +1,39 @@
+import path from 'node:path';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+
+import { DatabaseModule } from '../database/database.module';
+
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+
+import { CoursesService } from '../services/courses.service';
+import { StudentsService } from '../services/students.service';
+import { EnrollmentsService } from '../services/enrollments.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [
+    CoursesResolver,
+    StudentsResolver,
+    EnrollmentsResolver,
+
+    CoursesService,
+    StudentsService,
+    EnrollmentsService,
+  ],
+})
+export class HttpModule {}
---
+++ lab-04/src/app/entities/notification.ts
@@ -0,0 +1,82 @@
+import { randomUUID } from 'node:crypto';
+
+import { Replace } from '@helpers/Replace';
+import { Content } from './content';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  createdAt: Date;
+  readAt?: Date | null;
+  canceledAt?: Date | null;
+}
+
+export class Notification {
+  private _id: string;
+  private props: NotificationProps;
+
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId() {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category() {
+    return this.props.category;
+  }
+
+  public get createdAt() {
+    return this.props.createdAt;
+  }
+
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+}
